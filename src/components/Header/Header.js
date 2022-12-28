3f6b286b3517c10b1b3e4cd05f4cdf79dd8963db
sagarxoxo <mss82605@gmail.com>
2022-12-28 15:15:19 +0530

    backgrond pattern added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,42 @@
-import { AppBar, Box, Button, FormControlLabel, FormGroup, IconButton, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import { MaterialUISwitch } from '../../GlobalstylesComponents/Switch';
-import logo from "../../images/code.png"
-import "./Header.css"
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ParticlesBg from "particles-bg";
+import React from "react";
+import { MaterialUISwitch } from "../../GlobalstylesComponents/Switch";
+import logo from "../../images/code.png";
+import "./Header.css";
 export default function Header() {
-
-  const navItems = ['Portfolio', 'Technologies', 'About'];
-  
+  const navItems = ["Portfolio", "Technologies", "About"];
 
   return (
-    <Box sx={{ flexGrow: 1 }} >
-      <AppBar position="static" className="appNavbar" sx={{backgroundColor: "#0F1624"}}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        className="appNavbar"
+        sx={{ backgroundColor: "#0F1624" }}
+      >
         <Toolbar className="tollBar">
-        
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: "400", display: "flex", alignItems: "center" }}>
-          <img src={logo} className="logo"/> Web Developer
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              fontWeight: "400",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <img src={logo} className="logo" /> Web Developer
           </Typography>
-          <Box className=''>
+          <Box className="">
             {navItems.map((item) => (
               <Button key={item} className="menu" href={`#${item}`}>
                 {item}
@@ -25,6 +45,7 @@ export default function Header() {
           </Box>
         </Toolbar>
       </AppBar>
+      <ParticlesBg type="cobweb" color="blue" bg={true} />
     </Box>
-  )
+  );
 }
